88ac24148238dadd58a5fbb90223623888a6d65b
phperHuang <hard18771478203@163.com>
2019-07-19 11:02:20 +0800

    add registerController

---
+++ app/Http/Controllers/User/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RegisterController extends Controller
+{
+    //
+
+    public function register()
+    {
+
+    }
+
+    public function registerResult()
+    {
+
+    }
+
+}
